0905c6f416dee1a6521cb4351f4e9bf6c022ced5
aleksandar.petkov <aleksandar.petkov.sirma.bg>
2024-03-29 09:31:48 +0200

    Implement initial codebase

---
+++ portfolio/src/about/subheadingsData.js
@@ -0,0 +1,90 @@
+const subheadingsData = {
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+            I am an experienced Software Developer with over 3 years of
+            dedicated focus on backend development using the Spring Boot
+            framework. I specialize in backend workflow, Security, RESTful
+            APIs, and database management.
+          </p>
+          <p>
+            Possesses practical knowledge of frontend development, particularly
+            in JavaScript and React, enabling effective collaboration with
+            frontend teams."
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "Frameworks",
+      content: (
+        <>
+          <p>
+            Spring Boot, Spring WebFlux, Spring Data (JPA/Hibernate), Spring
+            Security, Spring Cloud AWS(Dynamo DB)
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "Program languages",
+      content: (
+        <>
+          <p>Java (versions 8 to 21), JavaScript, SQL</p>
+        </>
+      ),
+    },
+    {
+      title: "Database",
+      content: (
+        <>
+          <p>MySQL, PostgreSQL, SAP HANA, Dynamo DB (Amazon)</p>
+        </>
+      ),
+    },
+    {
+      title: "Cloud Technologies and Containerization",
+      content: (
+        <>
+          <p>
+            Docker, AWS(CloudWatch, IAM, Lambda, Amazon EventBridge, DynamoDB,
+            S3, API Gateway)
+          </p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "Software developer",
+      content: (
+        <>
+          <p>Company: Sirma Solutions (2022-present)</p>
+        </>
+      ),
+    },
+    {
+      title: "Junior Java developer",
+      content: (
+        <>
+          <p>Company: Accenture Bulgaria EOOD (2019-2021)</p>
+        </>
+      ),
+    },
+    {
+      title: "Intern Java developer",
+      content: (
+        <>
+          <p>Company: Accenture Bulgaria EOOD (2019)</p>
+        </>
+      ),
+    },
+  ],
+};
+
+export default subheadingsData;
---
+++ portfolio/src/skills/skillsData.js
@@ -0,0 +1,45 @@
+const skills = {
+  1: [
+    {
+      title: "Spring Ecosystem",
+      level: 5,
+    },
+    {
+      title: "Java Versions 8-21",
+      level: 5,
+    },
+    {
+      title: "Relational Databases",
+      level: 5,
+    },
+    {
+      title: "NoSQL Familiarity",
+      level: 3,
+    },
+    {
+      title: "Cloud Technologies",
+      level: 2,
+    }
+  ],
+  2: [
+    {
+      title: "HTML",
+      level: 1,
+    },
+    {
+      title: "CSS",
+      level: 1,
+    },
+    {
+      title: "JavaScript",
+      level: 3,
+    },
+    {
+      title: "React JS",
+      level: 2,
+
+    },
+  ],
+};
+
+export default skills;
